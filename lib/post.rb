6e09278853f8aa2cb8a60e94cd16519cfe9f584f
github username <dekadekadeka@gmail.com>
2019-04-25 20:54:58 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+    attr_accessor :author, :title
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def author_name
+        author.name if author
+    end
+
+    def self.all
+        @@all
+    end
+end
