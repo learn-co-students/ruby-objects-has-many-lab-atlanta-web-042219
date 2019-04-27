5286fe1a5c8b535c7eb3946ca4f654c558e554b6
Paula Tulis <tulis.paula@gmail.com>
2019-04-27 11:00:22 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Artist
+  @@song_count = 0
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+#self keyword to refer to the artist
+#on which we are calling this method.
+#We call the #artist= method on the song that is being passed in as an argument
+#and set it equal to self––the artist.
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
